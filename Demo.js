08063a63c0c2b4aa1ab612c9db62dd185333b185
Cedric Baaklini <cabaaklini@icloud.com>
2023-08-23 01:21:37 -0400

    Adding the JS file to the repo.

---
+++ Demo.js
@@ -0,0 +1,14 @@
+function add(x, y) {
+    return x + y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+var myName = "Cedric";
+console.log("I love TypeScript!");
